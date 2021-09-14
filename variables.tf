54831523fbdb4d5077153e94d5c400eda4030a3a
Carsten Duch <carsten@hashicorp.com>
2021-09-14 12:19:50 +0200

    initial commit

---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "name" {
+  type = string
+}
+
+variable "prefix" {
+  type = string
+}
+variable "suffix" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "owner" {
+  type    = string
+  description = "Owner which will be added as a tag to the resource group."
+}
+
+
+variable "admin_username" {
+  type        = string
+  description = "Administrator user name for virtual machine"
+}
+
+variable "admin_password" {
+  type        = string
+  description = "Password must meet Azure complexity requirements"
+}
