8850047d54cffa3b7984b206e8170d6afea5ed90
Carsten Duch <carsten@hashicorp.com>
2021-09-14 12:50:11 +0200

    changed portfreigabe

--- main.tf
+++ main.tf
@@ -69,13 +69,13 @@ resource "azurerm_network_security_group" "nsg" {
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "web"
+    name                       = "jenkins"
     priority                   = 1002
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
-    destination_port_range     = "80"
+    destination_port_range     = "8800"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
